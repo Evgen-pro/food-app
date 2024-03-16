d692676f7863629c75971ef08795c047e4fd9b8f
Yauheni Pratasevich <evgen.polotsk@gmail.com>
2024-03-16 13:15:30 +0300

    feat: add get/post request

--- js/script.js
+++ js/script.js
@@ -216,5 +216,58 @@ document.addEventListener('DOMContentLoaded', () => {
         '.menu .container',
         'menu__item'
     ).render()
+
+    //forms
+
+    const forms = document.querySelectorAll('form')
+    const message = {
+        loading: 'Loading',
+        success: 'Thanks, we will call you soon',
+        failure: 'whats wrong...'
+    }
+
+    forms.forEach(item => {
+        postData(item)
+    })
+
+
+    function postData(form) {
+        form.addEventListener('submit', (event) => {
+            event.preventDefault()
+
+            const statusMessage = document.createElement('div')
+            statusMessage.classList.add('status')
+            statusMessage.textContent = message.loading
+            form.append(statusMessage)
+
+            const request = new XMLHttpRequest()
+            request.open('POST', './server.php')
+            request.setRequestHeader('Content-type', 'application/json')
+            const formData = new FormData(form)
+
+            const object = {}
+            formData.forEach(function(value, key){
+                object[key] = value
+            })
+
+            const json = JSON.stringify(object)
+
+            request.send(json)
+
+            request.addEventListener('load', () => {
+                if (request.status == 200) {
+                     console.log(request.response)
+                     statusMessage.textContent = message.success;
+                     form.reset()
+                     setTimeout(() => {
+                        statusMessage.remove()
+                     }, 2000)
+                } else {
+                     statusMessage.textContent = message.failure
+
+                }
+            })
+        })
+    }
 })
 
