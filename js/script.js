4586e5fd90d575b7cd54fade2eda96ff62db267d
Yauheni Pratasevich <evgen.polotsk@gmail.com>
2024-05-14 20:42:25 +0300

    feat: fix errors

--- js/script.js
+++ js/script.js
@@ -323,8 +323,48 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 4000)
     }
 
-    fetch('http://localhost:3000/menu')
-        .then(data => data.json())
-        .then(res => console.log(res))
+    // fetch('http://localhost:3000/menu')
+    //     .then(data => data.json())
+    //     .then(res => console.log(res))
+
+
+    //slider
+
+    const slides = document.querySelectorAll('.offer__slide')
+    const prev = document.querySelector('.offer__slider-prev')
+    const next = document.querySelector('.offer__slider-next')
+    const total = document.querySelector('#total')
+    const current = document.querySelector('#current')
+    let slideIndex = 1
+
+    showSlides(slideIndex)
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`
+    }
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1
+        }
+        if (n < 1) {
+            slideIndex = slides.length
+        }
+
+        slides.forEach(item => item.style.display = 'none')
+        slides[slideIndex -1].style.display = 'block'
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n)
+    }
+
+    prev.addEventListener('click', () => {
+        plusSlides(-1)
+    })
+
+    next.addEventListener('click', () => {
+        plusSlides(1)
+    })
 })
 
